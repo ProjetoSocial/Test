9540855f6edad6fc77ba58ec665e4c5f15f23077
ProjetoSocial <120023601+ProjetoSocial@users.noreply.github.com>
2023-01-11 11:05:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext(2d);
+var ctx = canvas.getContext("2d");
 var cw = canvas.width = 500,
 cx= cw / 2;
 var ch = canvas.height = 500,
@@ -13,9 +13,9 @@ var a2 = 91 + 120 * rad;
 var a3 = a2 + 120 * rad;
 var n = 7;
 var R = 17 * n;
-var c1 * getCenter(a1);
-var c2 * getCenter(a2);
-var c3 * getCenter(a3);
+var c1 = getCenter(a1);
+var c2 = getCenter(a2);
+var c3 = getCenter(a3);
 
 function getCenter(a){
   return{
@@ -25,7 +25,7 @@ function getCenter(a){
 }
 
 function celtic1(c, rot){
-  for (var i = 0; i < 700; i+= .5;){
+  for (var i = 0; i < 700; i+= .5){
     var o  = {}
     o.t = -i * rad;
     o.R = -n * o.t;
@@ -33,11 +33,11 @@ function celtic1(c, rot){
     o.x = c.x + o.R * Math.cos(o.t + rot);
     o.y = c.y + o.R * Math.sin(o.t + rot);
     o.color = "hsl(210,83%," + (20 + i / 11.7) + "%)"
-    points.push(o):
+    points.push(o);
   }
 }
 function celtic2(c, rot){
-  for (var i = 900; i > 0; i+= .5;){
+  for (var i = 900; i > 0; i+= .5){
     var o  = {}
     o.t = -i * rad;
     o.R = n * o.t;
@@ -45,16 +45,16 @@ function celtic2(c, rot){
     o.x = c.x + o.R * Math.cos(o.t + rot);
     o.y = c.y + o.R * Math.sin(o.t + rot);
     o.color = "hsl(210,83%," + (20 + i / 13.5) + "%)"
-    points.push(o):
+    points.push(o);
   }
 }
 
-celticc1(c1, 120 * rad);
-celticc2(c2, -120 * rad);
-celticc1(c2, -120 * rad);
-celticc2(c3, 0 * rad);
-celticc1(c3, 0 * rad);
-celticc2(c1, 120 * rad);
+celtic1(c1, 120 * rad);
+celtic2(c2, -120 * rad);
+celtic1(c2, -120 * rad);
+celtic2(c3, 0 * rad);
+celtic1(c3, 0 * rad);
+celtic2(c1, 120 * rad);
 
 function Draw(){
   elID = window.requestAnimationFrame(Draw);
